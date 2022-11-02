8a9149b0922aa158b8b76ff1797d093642356956
jkim2211 <jk2211@georgetown.edu>
2022-11-02 17:27:46 -0400

    Update unemployment.py

--- app/unemployment.py
+++ app/unemployment.py
@@ -1,11 +1,15 @@
 
-# this is the "app/unemployment_report.py" file
+# this is the "app/unemployment_report.py" file...
+
 import json
 from pprint import pprint
+from statistics import mean
 
 import requests
+from plotly.express import line
 
 from app.alpha import API_KEY
+
 
 
 request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
@@ -13,35 +17,39 @@ request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={
 response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
-print(type(parsed_response))
-pprint(parsed_response)
+#print(type(parsed_response))
+#pprint(parsed_response)
+
+data = parsed_response["data"]
 
 # Challenge A
 #
-# What is the most recent unemployment rate? And the corresponding date? 
+# What is the most recent unemployment rate? And the corresponding date?
 # Display the unemployment rate using a percent sign.
 
-print("-------------------------")
-print("LATEST UNEMPLOYMENT RATE:")
-#print(data[0])
-print(f"{data[0]['value']}%", "as of", data[0]["date"])
+#breakpoint()
 
-#Challenge B
+latest = data[0]
+print(latest)
+
+# Challenge B
 #
-#What is the average unemployment rate for all months during this calendar year? How many months are included?
-from statistics import mean
+# What is the average unemployment rate for all months during this calendar year?
+# ... How many months does this cover?
+
 this_year = [d for d in data if "2022-" in d["date"]]
 
 rates_this_year = [float(d["value"]) for d in this_year]
+#print(rates_this_year)
+
 print("-------------------------")
 print("AVG UNEMPLOYMENT THIS YEAR:", f"{mean(rates_this_year)}%")
 print("NO MONTHS:", len(this_year))
 
-# Challenge C
-# 
-# Plot a line chart of unemployment rates over time.
 
-from plotly.express import line
+# Challenge C
+#
+# Plot a line chart of unemployment rates over time.
 
 dates = [d["date"] for d in data]
 rates = [float(d["value"]) for d in data]
