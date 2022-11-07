5f7d0ec67a745da3b5aa68a17234e4a33bd53aca
jkim2211 <jk2211@georgetown.edu>
2022-11-07 18:08:31 -0500

    Update stocks.py

--- app/stocks.py
+++ app/stocks.py
@@ -1,37 +1,3 @@
-# this is the "test/stocks_test.py" file...
-
-from app.stocks import format_usd, fetch_stocks_data
-
-from pandas import DataFrame
-
-
-
-def test_usd_formatting():
-
-    #assert 2 + 2 == 4
-
-    assert format_usd(4.5) == "$4.50"
-
-    assert format_usd(4.5555555) == "$4.56"
-
-    assert format_usd(1234567890) == "$1,234,567,890.00"
-
-    #assert format_usd("OOPS") == "______"
-
-
-
-def test_data_fetching():
-    result = fetch_stocks_data("NFLX")
-    assert isinstance(result, DataFrame)
-
-    assert "timestamp" in result.columns
-    assert "adjusted_close" in result.columns
-    assert "high" in result.columns
-    assert "low" in result.columns
-
-
-    assert len(result) >= 100
-5:53
 # this is the "app/stocks.py" file...
 
 
@@ -81,3 +47,4 @@ if __name__ == "__main__":
 
     print("HIGH:", format_usd(df["high"].max()))
     print("LOW:", format_usd(df["low"].min()))
+    
