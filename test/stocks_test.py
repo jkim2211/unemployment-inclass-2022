65d4432dd957731a9dd5e24b2baf58f436de5e67
jkim2211 <jk2211@georgetown.edu>
2022-11-09 18:01:22 -0500

    new md

--- test/stocks_test.py
+++ test/stocks_test.py
@@ -1,21 +1,36 @@
+# this is the "test/stocks_test.py" file...
 
-from app.stocks import format_usd,fetch_stocks_data
+from app.stocks import format_usd, fetch_stocks_data
+
 from pandas import DataFrame
 
 
+
 def test_usd_formatting():
-    assert 2+2 == 4
-    assert format_usd(4.5)=="$4.50"
-    assert format_usd(4.5555555)=="$4.56"
-    assert format_usd(1234567890)=="1,234,567,890.00"
+
+    #assert 2 + 2 == 4
+
+    assert format_usd(4.5) == "$4.50"
+
+    assert format_usd(4.5555555) == "$4.56"
+
+    assert format_usd(1234567890) == "$1,234,567,890.00"
+
+    #assert format_usd("OOPS") == "______"
+
+
 
 def test_data_fetching():
-    result=fetch_stocks_data("NFLX")
-    assert isinstance(result,DataFrame)
+    result = fetch_stocks_data("NFLX")
+    assert isinstance(result, DataFrame)
 
     assert "timestamp" in result.columns
     assert "adjusted_close" in result.columns
     assert "high" in result.columns
     assert "low" in result.columns
 
-    assert len(result)>=100
+    assert len(result) >= 100
+
+
+    #result = fetch_stocks_data("OOPS")
+    # todo: test invalid inputs. what should happen?
